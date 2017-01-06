8b77b62552d0c8f751abf1d10e66e06f5d681b4a
monicaselas <monicaselas15@gmail.com>
2017-01-06 15:52:05 +0530

    This is  my default constructor program

---
+++ Student2.java
@@ -0,0 +1,25 @@
+package com.niit;
+
+public class Student2 {
+	
+		int id;
+		String name;
+		 public  Student2()
+		{
+		
+		}
+		 public void display()
+		 {
+			 System.out.println( "id:"+id+" "+"name:"+name);
+		 }
+		 public static void main(String[] args)
+		 {
+			 Student2 obj=new Student2();
+			 obj.display();
+		 }
+	}
+		
+
+
+
+ 
